158798f7b4f4bc81fedf76f9a2650cf06f97e242
Fitzgerald-08 <epsilon.33@tutamail.com>
2026-07-27 13:29:59 -0700

    adding if-name control and file restructured

--- files.py
+++ files.py
@@ -12,56 +12,74 @@ import sys
 # --> sys (currently using)
 
 # Change the path to whatever is desired
-p = Path.home()
+p = Path("/home/marshall/")
 
-dirs = []
-files = []
-
-for file in p.iterdir():
-    if file.is_dir():
-        dirs.append(file.name)
-    if file.is_file():
-        files.append(file.name)
-
-# Add the contents of both lists to one list, which is going to be saved
-# to a file.
-
-dir_contents = dirs + files
-
-if 0 == len(dirs) == len(files):
-    print("[!] No directories nor files have been found...") 
-    sys.exit(1)
-else:
-    if len(dirs) == 0:
-        print("[!] No directories have been found...")
+# Show the results of the file scanning
+def show_results(dirs, files, dir_contents):
+    if 0 == len(dirs) == len(files):
+        print("[!] No directories nor files have been found...") 
+        sys.exit(1)
     else:
-        print("+ DIRECTOIRES FOUND:")
-        for d in dirs:
-            print(d)
+        if len(dirs) == 0:
+            print("[!] No directories have been found...")
+        else:
+            print("+ DIRECTOIRES FOUND:")
+            for d in dirs:
+                print(d)
 
-    if len(files) == 0:
-        print("\n[!] No files have been found...")
+        if len(files) == 0:
+            print("\n[!] No files have been found...")
+        else:
+            print("\n+ FILES FOUND:")
+            for f in files:
+                print(f)
+
+            answer = ""
+
+
+def main():
+    # Pass both lists and their addition to the show_results function
+    dirs = []
+    files = []
+
+    try:
+        for file in p.iterdir():
+            if file.is_dir():
+                dirs.append(file.name)
+            if file.is_file():
+                files.append(file.name)
+    except PermissionError as e:
+        print(f"An error has occured...\n{e}")
+        sys.exit(1)
     else:
-        print("\n+ FILES FOUND:")
-        for f in files:
-            print(f)
+        print("[+] Directories read correctly")
 
-        answer = ""
-        while answer != "n" or answer != "y":
-            answer = input("Would you like to save this information to a file? [y/n]")
-            if answer == "y":
-                print("[+] Saving to file...")
+    results = ""
+    while True:
+        results = input("[V]iew results\n[S]ave to file\ninput: ").lower()
 
-                new_file = Path.cwd() / "data_files.txt"
-                new_file.touch(mode=0o640)
+        if results == "s":
 
-                for line in dir_contents:
-                    file_contents = new_file.read_text()
-                    new_file.write_text(f"{file_contents}{line}\n")
+            # Introduce the name for the file to store results
+            file_name = input("Introduce file name: ")
+            file_path = p / file_name
+            Path.touch(f"{file_path}.txt", mode=0o644)
 
-                sys.exit(0)
-            elif answer == "n":
-                print("Exiting script...")
-                sys.exit(0)
-            else:
-                print("[!] Fatal: No valid answer was detected")
+            # Fill the file with the results
+            results = dirs + files
+            with file_path.open(mode="a") as f:
+                for entry in results:
+                    f.write(f"{entry}\n")
+
+            # Exit successfully
+            sys.exit(0)
+        elif results == "v":
+            dir_contents = dirs + files
+            show_results(dirs, files, dir_contents)
+            sys.exit(0)
+        else:
+            print("[!] Input error. Specify S/V")
+
+
+if __name__ == "__main__":
+    main()
